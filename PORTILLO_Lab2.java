ff51af4811aaed3df720ca54f4551298593987d8
Portillocutie <portilloabel752@gmail.com>
2024-09-03 13:40:55 +0800

    Add files via upload

---
+++ PORTILLO_Lab2.java
@@ -0,0 +1,33 @@
+public class CylinderApp {
+    public static void main(String[] args) {
+        CylinderVol cyl = new CylinderVol();
+        cyl.input(9, 18);  // Example values for radius and height
+        cyl.area();
+        cyl.volume();
+    }
+}
+
+class Cylinder {
+    double radius;
+    double height;
+    double area;
+
+    void input(double r, double h) {
+        radius = r;
+        height = h;
+    }
+
+    void area() {
+        area = 2 * Math.PI * radius * radius + 2 * Math.PI * radius * height;
+        System.out.println("Area of the cylinder: " + area);
+    }
+}
+
+class CylinderVol extends Cylinder {
+    double volume;
+
+    void volume() {
+        volume = Math.PI * radius * radius * height;
+        System.out.println("Volume of the cylinder: " + volume);
+    }
+}
